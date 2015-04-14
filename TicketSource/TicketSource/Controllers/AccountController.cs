30990c6e84a10c4d9a73f90d23e06f303bf029b5
= <=>
2015-04-13 23:19:01 -0400

    HomePage Styling and DB Addition

--- TicketSource/TicketSource/Controllers/AccountController.cs
+++ TicketSource/TicketSource/Controllers/AccountController.cs
@@ -151,7 +151,12 @@ namespace TicketSource.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email,
+                                                 Email = model.Email,
+                                                 FirstName = model.FName,
+                                                 LastName = model.LName
+                                               };
+
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- TicketSource/TicketSource/Models/AccountViewModels.cs
+++ TicketSource/TicketSource/Models/AccountViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace TicketSource.Models
@@ -76,6 +77,11 @@ namespace TicketSource.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Are you a student?")]
+        public Boolean IsStudent { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
